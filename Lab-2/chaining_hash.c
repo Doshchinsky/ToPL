469e0d57bb17a0de659a8eed24c2fd1cf8b1f815
Doshinsky <akter1997@gmail.com>
2018-05-06 22:42:39 +0700

    Added collision detection and fixing of it

--- Lab-2/chaining_hash.c
+++ Lab-2/chaining_hash.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include "chaining_hash.h"
 
 void hashtab_init(struct listnode **hashtab)
@@ -11,11 +8,69 @@ void hashtab_init(struct listnode **hashtab)
 	}
 }
 
-unsigned int hashing_hash(char *key)
+unsigned int hashtab_hash(char *key)
 {
 	unsigned int h = 0;
 	char *p;
 	for (p = key; *p != '\0'; p++)
 		h = h * HASHTAB_MUL + (unsigned int) * p;
 	return h % HASHTAB_SIZE;
+}
+
+void hashtab_add(struct listnode **hashtab, char *key, int value)
+{
+	struct listnode *node;
+	if (hashtab_lookup(hashtab, key)) 
+	{
+		int index = hashtab_hash(key);
+		node = malloc(sizeof(*node));
+		if (node != NULL) 
+		{
+			node->key = strdup(key);
+			node->value = value;
+			node->coll = hashtab[index];
+			hashtab[index] = node;
+		}
+		return;	
+	}
+	int index = hashtab_hash(key);
+	
+	// Вставка в начало списка
+	node = malloc(sizeof(*node));
+	if (node != NULL) {
+		node->key = strdup(key);
+		node->value = value;
+		node->next = hashtab[index];
+		hashtab[index] = node;
+	}
+}
+
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key)
+{
+	int index;
+	struct listnode *node;
+	index = hashtab_hash(key);
+	if (hashtab[index] == NULL) return NULL;
+	if (strcmp(hashtab[index]->key, key) == 0) {
+		return hashtab[index];
+	}
+	for (int i = (index + 1) % HASHTAB_SIZE; i != index; i = (i + 1) % HASHTAB_SIZE){
+		if (hashtab[i] == NULL) continue;
+		if (strcmp(hashtab[i]->key, key) == 0)
+			return node;
+	}
+	return NULL;
+}
+
+void hashtab_delete(struct listnode **hashtab)
+{
+	struct listnode *p, *prev;
+	for (int i = 0; i < HASHTAB_SIZE ; ++i) {
+		for (p = hashtab[i]; p != NULL;) {
+			if (p->key) free(p->key);
+			prev = p;
+			p = p->next;
+			free(prev);
+		}
+	}
 }
--- Lab-2/chaining_hash.h
+++ Lab-2/chaining_hash.h
@@ -1,6 +1,11 @@
 #ifndef CHAINING
 #define CHAINING
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 #define HASHTAB_MUL 31
 #define HASHTAB_SIZE 50
 
@@ -9,11 +14,15 @@ struct listnode
 	char *key;
 	int value;
 	struct listnode *next;
+	struct listnode *coll;
 };
 
 struct listnode* hashtab[HASHTAB_SIZE];
 
 void hashtab_init(struct listnode **hashtab);
-unsigned int hashing_hash(char *s);
+unsigned int hashtab_hash(char *key);
+void hashtab_add(struct listnode **hashtab, char *key, int value);
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key);
+void hashtab_delete(struct listnode **hashtab);
 
 #endif
